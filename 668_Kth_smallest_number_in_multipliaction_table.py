5026caa81f03de64ebf0b083ac3761dcb7497902
BBBBBBNJU <yanhao8510@gmail.com>
2017-12-08 14:51:50 +0800

    test

--- 668_Kth_smallest_number_in_multipliaction_table.py
+++ 668_Kth_smallest_number_in_multipliaction_table.py
@@ -1,1 +1,23 @@
-class
+class Solution(object):
+    def findKthNumber(self, m, n, k):
+        """
+        :type m: int
+        :type n: int
+        :type k: int
+        :rtype: int
+        """
+        def satisfy(x):
+            temp_sum = 0
+            for i in range(1, m+1):
+                temp_sum += min(x / i, n)
+            return temp_sum >= k
+            
+        low, high = 1, m*n
+        while(low < high):
+            mid = (low + high) / 2
+            if satisfy(mid):
+                high = mid
+            else:
+                low = mid + 1
+        return low
+            
