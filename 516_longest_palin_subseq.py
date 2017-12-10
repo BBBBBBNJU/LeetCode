ba71104ccb65b78410ae22b1610e5d243e4a785c
BBBBBBNJU <yanhao8510@gmail.com>
2017-12-10 20:26:37 +0800

    516 test

---
+++ 516_longest_palin_subseq.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def longestPalindromeSubseq(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        dp = [[0] * len(s)] * len(s)
+        for i in range(len(dp)-1, -1, -1):
+            dp[i][i] == 1
+            for j in range(i+1, len(dp[0]), 1):
+                if s[i] == s[j]:
+                    dp[i][j] = dp[i+1][j-1] + 2
+                else:
+                    dp[i][j] = max(dp[i+1][j], dp[i][j-1])
+        return dp[0][len(s)-1]
