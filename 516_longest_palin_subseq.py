b89dda9ff986c0d1ee37f9aa3af057ca2c036a75
BBBBBBNJU <yanhao8510@gmail.com>
2017-12-10 20:44:52 +0800

    516 test

--- 516_longest_palin_subseq.py
+++ 516_longest_palin_subseq.py
@@ -9,7 +9,10 @@ class Solution(object):
             dp[i][i] == 1
             for j in range(i+1, len(dp[0]), 1):
                 if s[i] == s[j]:
-                    dp[i][j] = dp[i+1][j-1] + 2
+                    if i + 1<= j - 1:
+                        dp[i][j] = dp[i+1][j-1] + 2
+                    else:
+                        dp[i][j] = 2
                 else:
                     dp[i][j] = max(dp[i+1][j], dp[i][j-1])
         return dp[0][len(s)-1]
